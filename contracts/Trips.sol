3444a7f7c7b80d7513828808f069381b2b65ea10
rubenchos6 <rubenrortega33@gmail.com>
2022-04-17 12:35:44 -0500

    Agregar estructura que contiene el pago

--- contracts/Trips.sol
+++ contracts/Trips.sol
@@ -5,8 +5,14 @@ contract Trips {
 	
 	//INITIAL VALUES
 	//RegisterInitialTrip
-	address[3][] public initialRegisterTrip;
-	event initT(address indexed _idPassenger, address indexed _idDriver, address indexed _idVehicle, uint _idRegister);
+	struct Trip{
+		address idPassenger;
+		address idDriver;
+		address idVehicle;
+		uint cost;
+	}
+	Trip[] initialRegisterTrip;
+	event initT(address indexed _idPassenger, address indexed _idDriver, address indexed _idVehicle, uint _idRegister, uint cost);
 	
 	//RegisterAPs
 	struct APTS{
@@ -14,35 +20,35 @@ contract Trips {
 		string ap;
 		string timestamp;
 	}
-	mapping(address => APTS[]) apts;
+	mapping(bytes32 => APTS[]) apts;
 	string public ap;
 	string public timestamp;
-	event registerAP(address indexed _tripTransaction, string _ap, string _timestamp, uint _idRegister);
+	event registerAP(bytes32 indexed _tripTransaction, string _ap, string _timestamp, uint _idRegister);
 	
 	//End Trip
 	struct TripReceipt{
 		uint cost;
 		uint indexTrip;
 	}
-	mapping(address => TripReceipt) receipts;
-	event endT(address indexed _tripTransaction, uint _cost, uint _indexTrip);
+	mapping(bytes32 => TripReceipt) receipts;
+	event endT(bytes32 indexed _tripTransaction, uint _cost, uint _indexTrip);
 	
 	
 	//FUNCTIONS
 	//Register initial Trip
-	function initTrip(address idDriver, address idVehicle) public returns (uint){
-		
+	function initTrip(address idDriver, address idVehicle, uint cost) public payable returns (uint){
 		//send eth to contract
+		require(msg.value >= 1 ether);
 		
 		uint idRegister = initialRegisterTrip.length;
-		initialRegisterTrip.push([msg.sender,idDriver,idVehicle]);
+		initialRegisterTrip.push(Trip(msg.sender,idDriver,idVehicle,cost));
 		
-		emit initT(msg.sender, idDriver, idVehicle,idRegister);
+		emit initT(msg.sender, idDriver, idVehicle,idRegister, cost);
 		return idRegister;
 	}
 	
 	//Register AP and timestamp
-	function regisAPandTime(address tripTransaction, string memory passedAp, string memory passedTimestamp,uint indexTrip) public returns (uint){
+	function regisAPandTime(bytes32 tripTransaction, string memory passedAp, string memory passedTimestamp,uint indexTrip) public returns (uint){
 		ap=passedAp;
 		timestamp=passedTimestamp;
 		
@@ -53,28 +59,27 @@ contract Trips {
 	}
 	
 	//End Trip
-	function endTrip(address tripTransaction, uint cost, uint indexTrip) public returns (uint){
+	function endTrip(bytes32 tripTransaction, uint indexTrip) public payable returns (uint){
+		require(msg.value >= 1 ether);
+		uint cost= initialRegisterTrip[indexTrip].cost;
+		
 		receipts[tripTransaction]=TripReceipt(cost,indexTrip);
-		address addressDriver=initialRegisterTrip[indexTrip][2];
-		
-		//send eth to address
-		
+		//address addressDriver=initialRegisterTrip[indexTrip][2];
 		emit endT(tripTransaction, cost, indexTrip);
 		return 0;
 	}	
 	
-	
 	//LOOK UP FUNCTIONS
-	function getInitialTripData(uint index) public view returns (address[3] memory){
+	function getInitialTripData(uint index) public view returns (Trip memory){
 		require(index>=0 && index<initialRegisterTrip.length);
 		return initialRegisterTrip[index];
 	}
 	
-	function getAPsInfo(address transaction) public view returns (APTS[] memory){
+	function getAPsInfo(bytes32 transaction) public view returns (APTS[] memory){
 		return apts[transaction];
 	}
 	
-	function getFinalTripData(address transaction) public view returns (TripReceipt memory){
+	function getFinalTripData(bytes32 transaction) public view returns (TripReceipt memory){
 		return receipts[transaction];
 	}
 	
--- migrations/3_deploy_registerAP.js
+++ migrations/3_deploy_registerAP.js
@@ -2,6 +2,6 @@ var RegisterAP = artifacts.require("./RegisterAP.sol");
 var Trips = artifacts.require("./Trips.sol");
 
 module.exports = function(deployer){
-	deployer.deploy(RegisterAP);
+	//deployer.deploy(RegisterAP);
 	deployer.deploy(Trips);
 };
